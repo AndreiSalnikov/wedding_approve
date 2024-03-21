d182edfba76f469942023a5a4280e5c45a55109c
Chijik <Ans762@mail.ru>
2024-03-21 19:12:01 +0300

    fix: fix to build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,18 @@ import Header from "@/components/Header/Header";
 import Welcome from "@/components/Welcome/Welcome";
 import Rsvp from "@/components/Rsvp/Rsvp";
 import Schedule from "@/components/Schedule/Schedule";
-import Map from  "@/components/Map/Map"
+import Map from "@/components/Map/Map"
+import {Suspense} from "react";
+
 export default function Home() {
     return (
         <>
             <Header/>
             <main>
                 <Welcome/>
-                <Rsvp/>
+                <Suspense>
+                    <Rsvp/>
+                </Suspense>
                 <Schedule/>
                 <Map/>
             </main>
